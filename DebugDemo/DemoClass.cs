5048677844e97defee87d4e8e65d51fe5130a190
CeciliaSHARP <cecilia.wiren@live.se>
2016-11-16 08:27:51 +0100

    more demos

---
+++ DebugDemo/DemoClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DebugDemo
+{
+
+    class DemoClass
+    {
+        public DemoClass2 Value { get; set; }
+        public DemoClass()
+        {
+            Value = new DemoClass2();
+        }
+        public void ThrowException()
+        {
+            throw new Exception();
+        }
+    }
+
+    public class DemoClass2
+    {
+        private DemoClass3 property;
+
+        public DemoClass3 Property()
+        {
+            return null;
+        }
+
+    }
+
+    public class DemoClass3
+    {
+        public int Text { get; set; }
+    }
+}
--- DebugDemo/Program.cs
+++ DebugDemo/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace DebugDemo
 {
@@ -10,6 +7,22 @@ namespace DebugDemo
     {
         static void Main(string[] args)
         {
+            string[] lines = { "First line", "Second line", "Third line" };
+
+            string mydocpath =
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            using (StreamWriter outputFile = new StreamWriter(mydocpath + @"\WriteLines.txt"))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            }
+
+            var d = new DemoClass();
+            d.ThrowException();
+            var t = d.Value.Property().Text;
         }
     }
+
+  
 }
