41abb1956dc9151633df51527527c95f94248f57
CeciliaSHARP <cecilia.wiren@live.se>
2017-01-30 22:54:20 +0100

    Add Test demo

---
+++ Calculator/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Sum { get; set; }
+
+        public void Add(int i)
+        {
+            Sum += i;
+        }
+
+        public void Substract(int i)
+        {
+            Sum -= 1;
+        }
+
+    }
+}
---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            var c = new Calculator();
+            c.Add(7);
+            Assert.AreEqual(7, c.Sum);
+        }
+
+        [TestMethod]
+        public void SubTest()
+        {
+            var c = new Calculator();
+            c.Substract(4);
+            Assert.AreEqual(-4, c.Sum);
+        }
+    }
+}
