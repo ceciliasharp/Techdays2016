3b27b6eb928798bab76ecc6dd23ed2c8aeda888d
CeciliaSHARP <cecilia.wiren@live.se>
2017-08-30 14:05:33 +0200

    update for 7.3

--- Editor/Demo1/refactor.cs
+++ Editor/Demo1/refactor.cs
@@ -4,13 +4,30 @@
     {
         public int Number { get; set; }
 
-        public void Test()
+        public void Inizilazing()
         {
             var c = new Person();
             c.Name = "Testson";
             c.Age = 13;
             c.Phone = "123345";
             c.HairColor = "green";
+
+        }
+
+        public void IfStatement(int i)
+        {
+            if (i == 1)
+            {
+                //something
+            }
+            else if (i == 2)
+            {
+                //something somthing
+            }
+            else if (i == 3)
+            {
+                //something somthing something
+            }
         }
     }
 }
